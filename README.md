73f78fd5f5db259e2c9616e3b4a89764931fed04
Oscar Meanwell <oscar.meanwell@gmail.com>
2018-02-01 12:00:48 +0000

    Bitbucket to git

---
+++ README.md
@@ -0,0 +1,18 @@
+# ANDROID GET LOCATION #
+
+People seem to overcomplicate how to get Android's Current/Last Known Location, so heres how.
+
+### Details ###
+
+* Uses Android Studio
+* Clone and run
+* Sorry I copied the whole package and all the rubbish, just to make it seemingly easy but probably more difficult.
+* Code is very simple, you can find details here - https://developer.android.com/training/location/retrieve-current.html#last-known
+
+### Whats not in the Main Activity file that I need? ###
+
+* Add this line to dependencies in the build.gradle compile 'com.google.android.gms:play-services:11.0.0'
+
+### Who do I talk to? ###
+
+* oscarmeanwell@gmail.com
---
+++ mySensors/app/src/main/java/com/example/a40201136/mysensors/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.a40201136.mysensors;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+public class MainActivity extends AppCompatActivity {
+    TextView txtThree;
+    private FusedLocationProviderClient mFusedLocationClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtThree = (TextView) findViewById(R.id.txtThree);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            txtThree.setText("Location is: "+location.getLatitude()+", "+location.getLongitude());
+                        }
+                    }
+                });
+    }
+}
